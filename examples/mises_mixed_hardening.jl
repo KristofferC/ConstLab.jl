a2311534ca0b619f582d6161fb99b98ab3b74c97
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-10-05 11:46:07 +0200

    update to use Voigt.jl

--- examples/mises_mixed_hardening.jl
+++ examples/mises_mixed_hardening.jl
@@ -2,6 +2,10 @@ using Parameters
 using ForwardDiff
 using NLsolve
 using ConstLab
+using Voigt
+using Voigt.Unicode
+
+using Devectorize
 
 import ConstLab.stress
 
@@ -22,128 +26,67 @@ end
     α_∞::Float64
 end
 
-const Id = Float64[1, 1, 1, 0, 0, 0, 0, 0, 0]
-
-const Idev = 1/3 * Float64[  2    -1    -1     0     0     0     0     0     0;
-                            -1     2    -1     0     0     0     0     0     0;
-                            -1    -1     2     0     0     0     0     0     0;
-                             0     0     0     3     0     0     0     0     0;
-                             0     0     0     0     3     0     0     0     0;
-                             0     0     0     0     0     3     0     0     0;
-                             0     0     0     0     0     0     3     0     0;
-                             0     0     0     0     0     0     0     3     0;
-                             0     0     0     0     0     0     0     0     3];
-
-function to_9comp(v::Vector, stress = false)
-    length(v) == 6 || throw(ArgumentError("Wrong length"))
-    if stress
-        f = 1.0
-    else
-        f = 2.0
-    end
-    v9 = [v; v[4]; v[5]; v[6]]
-    v9[4:end] /= f
-    v9
-end
-
-function to_6comp(v::Vector, stress = false)
-    length(v) == 9 || throw(ArgumentError("Wrong length"))
-    if stress
-        f = 1.0
-    else
-        f = 2.0
-    end
-    v6 = v[1:6]
-    v6[4:end] *= f
-    v6
-end
-
-function to_6comp(m::Matrix)
-    size(m) == (9,9) || throw(ArgumentError("Wrong size"))
-    m6 = m[1:6, 1:6]
-    m6[4:6, :] /= 2.0
-    m6
-end
+const II = veye(6) ⊗ veye(6)
+const Idev6 = eye(6,6) - 1/3 * II
 
 function stress(ε, dt, matpar::MisesMixedHardMP, matstat::MisesMixedHardMS)
 
-    @unpack_MisesMixedHardMP matpar
-    @unpack_MisesMixedHardMS matstat
-    ε = to_9comp(ε)
-    n_α_dev = to_9comp(n_α_dev, true)
-    n_ε_p = to_9comp(n_ε_p)
+    @unpack matpar: E, ν, σy, H, r, κ_∞, α_∞
+    @unpack matstat: n_ε_p, n_α_dev, n_κ, n_μ
 
     G = E / 2(1 + ν)
     K = E / 3(1 - 2ν)
-    Ee = 2 * G * Idev + K * (Id * Id')
+    Ee = 2 * G * Idev6 + K * II
+    Ee[4:6, 4:6] /= 2.0
     σ_tr = Ee * (ε - n_ε_p)
-    σ_dev_tr = Idev * σ_tr
-    σ_red_tr = σ_tr - n_α_dev
-    σ_red_dev_tr = Idev * σ_red_tr
+    σ_dev_tr = dev(σ_tr)
 
-    σ_red_e_tr = sqrt(3/2) * norm(σ_red_dev_tr)
+    σ_red_e_tr = sqrt(3/2) * vnorm(dev(σ_tr - n_α_dev))
 
     Φ = σ_red_e_tr - σy - n_κ
 
     if Φ < 0
-        ms = MisesMixedHardMS(to_6comp(n_ε_p), to_6comp(n_α_dev, true),
-                                       n_κ, n_μ)
-        return σ_tr[1:6], to_6comp(Ee), ms
+        ms = MisesMixedHardMS(n_ε_p, n_α_dev, n_κ, n_μ)
+        return σ_tr, Ee, ms
     else
-
-        # Initial guesses
-        σ_dev0 = σ_dev_tr
-        α_dev0 = n_α_dev
-        κ0 = n_κ
-        μ0 = n_μ
 
         # Takes the unknown state vector and σ_dev_para
         function res_wrapper(x, σ_dev_pr)
-            T = promote_type(eltype(x), eltype(σ_dev_pr))
-            R = zeros(T, length(x))
-            _σ_dev = x[1:9]
-            _α_dev = x[10:18]
-            _κ = x[19]
-            _μ = x[20]
+            _σ_dev = x[1:6]
+            _α_dev = x[7:12]
+            _κ = x[13]
+            _μ = x[14]
             R_σ, R_α, R_κ, R_Φ = compute_residual(_σ_dev, _α_dev, _κ, _μ, σ_dev_pr, matpar, matstat)
-            R[1:9] = R_σ
-            R[10:18] = R_α
-            R[19] = R_κ
-            R[20] = R_Φ
-            R
+            R = [R_σ; R_α; R_κ; R_Φ]
         end
 
         res_wrapper(x) = res_wrapper(x, σ_dev_tr)
 
-
         jac = jacobian(res_wrapper)
 
-        x0 = [σ_dev0; α_dev0; κ0; μ0]
-
-        res = nlsolve(not_in_place(res_wrapper, jac), x0; iterations = 30, store_trace = true, ftol = 1e-7)
+        # Initial guess
+        x0 = [σ_dev_tr; n_α_dev; n_κ; n_μ]
+        res = nlsolve(not_in_place(res_wrapper, jac), x0; iterations = 30, ftol = 1e-7)
         if !NLsolve.converged(res)
             error("No convergence in material routine")
         end
 
         X = res.zero
 
-        σ_dev_X = X[1:9]
-        α_dev_X = X[10:18]
-        κ_X = X[19]
-        μ_X = X[20]
-        σ_X = σ_tr - σ_dev_tr + σ_dev_X
-        σ_red_X = σ_X - α_dev_X
-        σ_red_dev_X = Idev * σ_red_X
-        σ_red_e_X = sqrt(3/2) * norm(σ_red_dev_X)
-        ε_p = n_ε_p + 3/2 * μ_X / σ_red_e_X * σ_red_dev_X
-        ε_p = to_6comp(ε_p)
-        α_dev_X = to_6comp(α_dev_X, true)
+        σ_dev_X = X[1:6]
+        α_dev_X = X[7:12]
+        κ_X = X[13]
+        μ_X = X[14]
+        @devec σ_X = σ_tr - σ_dev_tr + σ_dev_X
+        σ_red_dev_X = dev(σ_X - α_dev_X)
+        σ_red_e_X = sqrt(3/2) * vnorm(σ_red_dev_X)
+        ε_p = n_ε_p + 3/2 * μ_X ./ σ_red_e_X * σ_red_dev_X
 
         ms = MisesMixedHardMS(ε_p, α_dev_X, κ_X, μ_X)
 
         function dRdε_wrapper(ε_para)
             σ_ATS = Ee * (ε_para - n_ε_p)
-            σ_dev_ATS = Idev * σ_ATS
+            σ_dev_ATS = dev(σ_ATS)
             res_wrapper(X, σ_dev_ATS)
         end
 
@@ -152,28 +95,28 @@ function stress(ε, dt, matpar::MisesMixedHardMP, matstat::MisesMixedHardMS)
         dRdε_f = jacobian(dRdε_wrapper)
         dRdε = dRdε_f(ε)
         dXdε = -J \ dRdε
-        dσdev_dε = dXdε[1:9, 1:9]
+        dσdev_dε = dXdε[1:6, 1:6]
 
-        ATS = K * (Id * Id') + dσdev_dε
+        ATS = K * (veye(6) * veye(6)') + dσdev_dε
 
-       return σ_X[1:6], to_6comp(ATS), ms
+       return σ_X, ATS, ms
     end
 end
 
 function compute_residual(σ_dev, α_dev, κ, μ, σ_dev_tr, matpar, matstat)
 
-    @unpack_MisesMixedHardMP matpar
-    @unpack_MisesMixedHardMS matstat
+    @unpack matpar: E, ν, σy, H, r, κ_∞, α_∞
+    @unpack matstat: n_ε_p, n_α_dev, n_κ, n_μ
 
     G = E / 2(1 + ν)
 
-    σ_red_dev = σ_dev - α_dev
-    σ_red_e = sqrt(3/2) * norm(σ_red_dev)
+    @devec σ_red_dev = σ_dev - α_dev
+    σ_red_e = sqrt(3/2) * vnorm(σ_red_dev)
     σ_red_dev_hat = σ_red_dev / σ_red_e
 
-    R_σ = σ_dev - σ_dev_tr + 3.*G.*μ .* σ_red_dev_hat
+    @devec R_σ = σ_dev - σ_dev_tr + 3.*G.*μ .* σ_red_dev_hat
     R_κ = κ - n_κ - r * H * μ * (1 - κ / κ_∞)
-    R_α = α_dev - to_9comp(n_α_dev, true) - (1-r) .* H .* μ .* (σ_red_dev_hat - 1./α_∞ .* α_dev)
+    @devec R_α = α_dev - n_α_dev - (1-r) .* H .* μ .* (σ_red_dev_hat - 1./α_∞ .* α_dev)
     R_Φ = σ_red_e - σy - κ
 
     return R_σ, R_α, R_κ, R_Φ
--- examples/mises_viscoplast.jl
+++ examples/mises_viscoplast.jl
@@ -1,9 +1,13 @@
 using Parameters
 using ForwardDiff
 using NLsolve
+using Devectorize
 
+using Voigt
+using Voigt.Unicode
+
+using ConstLab
 import ConstLab.stress
-import Base.show
 
 @with_kw immutable ViscoPlastMS <: MatStatus
     n_ε_p::Vector{Float64} = zeros(6)
@@ -21,21 +25,11 @@ end
     tstar::Float64
 end
 
-const Id = Float64[1, 1, 1, 0, 0, 0]
-
-const Idev6 = 1/3 * Float64[ 2    -1    -1     0     0    0;
-                            -1     2    -1     0     0     0 ;
-                            -1    -1     2     0     0     0 ;
-                             0     0     0     3     0     0 ;
-                             0     0     0     0     3     0 ;
-                             0     0     0     0     0     3 ];
-
-const Ihalf = diagm([1, 1, 1, 0.5, 0.5, 0.5]);
+const II = veye(6) ⊗ veye(6)
+const Idev6 = eye(6,6) - 1/3 * II
+const Ihalf = diagm([1, 1, 1, 0.5, 0.5, 0.5])
 
 const diff_cache = ForwardDiffCache()
-
-norm_voigt(σ) = sqrt(σ[1]^2 + σ[2]^2 + σ[3]^2 +
-                     2σ[4]^2 + 2σ[5]^2 + 2σ[6]^2)
 
 function stress(ε, dt, matpar::ViscoPlastMP, matstat::ViscoPlastMS)
     @unpack_ViscoPlastMP matpar
@@ -43,16 +37,18 @@ function stress(ε, dt, matpar::ViscoPlastMP, matstat::ViscoPlastMS)
 
     G = E / 2(1 + ν)
     K = E / 3(1 - 2ν)
-    Ee = 2 * G * Ihalf * Idev6 + K * (Id * Id')
+    Ee = 2 * G * Idev6 + K * II
+    Ee[4:6, 4:6] /= 2
 
     σ_tr = Ee * (ε - n_ε_p)
-    σ_dev_tr = Idev6 * σ_tr
-    s_tr = sqrt(3/2) * norm_voigt(σ_dev_tr)
+    σ_dev_tr = dev(σ_tr)
+    s_tr = sqrt(3/2) * vnorm(σ_dev_tr)
 
     Φ = s_tr - σy - n_κ
     if Φ < 0
         return σ_tr, Ee, ViscoPlastMS(n_ε_p, n_κ, n_μ)
     else
+
         function res_wrapper(x)
             s_res = x[1]
             κ_res = x[2]
@@ -62,30 +58,27 @@ function stress(ε, dt, matpar::ViscoPlastMP, matstat::ViscoPlastMS)
         end
 
         # Initial guesses
-        x0 = [s_tr;
-              n_κ;
-              n_μ]
+        x0 = [s_tr, n_κ, n_μ]
 
         jac = jacobian(res_wrapper)
 
-        res = nlsolve(not_in_place(res_wrapper, jac), x0; iterations = 20, ftol=1e-5, store_trace = true)
+        res = nlsolve(not_in_place(res_wrapper, jac), x0; iterations = 20, ftol=1e-5)
         X = res.zero::Vector{Float64}
 
         s = X[1]
         κ = X[2]
         μ = X[3]
 
-
         σ_dev = 1 / (1 + 3G * μ / s) * σ_dev_tr
-        σ = σ_tr - σ_dev_tr + σ_dev
+        @devec σ = σ_tr - σ_dev_tr + σ_dev
         ε_p = n_ε_p + 3 / 2s * μ * σ_dev
 
         # ATS
         G_primprim =  n / σc * (Φ / σc)^(n-1)
         h_at = 3 * G + H + 1 / G_primprim * tstar / dt
         b = (3 * G * μ / s) / (1 + 3 * G * μ / s)
-        Q = Idev6*Ihalf  - (3/(2*s^2) * (σ_dev * σ_dev'))
-        ATS = Ee + (- 2 * G * b * Q - 9 * G^2 / (h_at * s^2) * (σ_dev * σ_dev'))
+        Q = Idev6 * Ihalf  - (3/(2*s^2) * (σ_dev ⊗ σ_dev))
+        ATS = Ee + (- 2 * G * b * Q - 9 * G^2 / (h_at * s^2) * (σ_dev ⊗ σ_dev))
 
        return σ, ATS, ViscoPlastMS(ε_p, κ, μ)
     end
@@ -96,14 +89,14 @@ function compute_residual(s, κ, μ, s_tr, dt, matpar, matstat)
     @unpack_ViscoPlastMS matstat
 
     G = E / 2(1 + ν)
-    Φ = s - σy - κ
-    G_primprim =  n / σc * (max(0, Φ) / σc)^(n-1);
+    Φ = max(0, s - σy - κ)
+    G_primprim =  n / σc * (Φ / σc)^(n-1)
 
     R_s = s - s_tr + 3G*μ
 
     R_κ = κ - n_κ - H * μ
 
-    G_prim = (max(0, Φ) / σc)^n;
+    G_prim = (Φ / σc)^n;
     R_μ = G_prim - tstar / dt * μ
 
     return R_s / 3G, R_κ / H, R_μ / G_primprim
